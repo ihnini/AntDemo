98a56641624a7d1cf34d6d60e08e0f1ec259c109
ihnini <hniniibrahim@yahoo.fr>
2020-03-03 09:20:29 +0100

    add techs display

--- src/App.java
+++ src/App.java
@@ -4,5 +4,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("E-challenge 2020 - MARS Session");
+
+        String[] techs = new String[]{"mvn", "ant", "git"};
+
+        for (String tech : techs) {
+            System.out.println("--> "+tech);
+        }
     }
 }
